2338f7f4e3ab646b464eb49723a6e3516fdd7df2
awp258 <aiwenping8@163.com>
2019-10-21 17:52:43 +0800

    Initial commit

---
+++ permission/src/main/java/com/abraham/Permissions.java
@@ -0,0 +1,12 @@
+package com.abraham;
+
+/**
+ * Enum class to handle the different states
+ * of permissions since the PackageManager only
+ * has a granted and denied state.
+ */
+enum Permissions {
+    GRANTED,//同意
+    DENIED, //拒绝
+    NOT_FOUND//未找到
+}
